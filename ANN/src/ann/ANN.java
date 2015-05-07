d5a30ad2169e8dfaad6371ddf4d35028e02d9416
adam aulia rahmadi <adamaulia242@gmail.com>
2015-05-08 00:01:39 +0700

    training udah jadi, tapi mse masih naik terus -_ -

--- ANN/src/ann/ANN.java
+++ ANN/src/ann/ANN.java
@@ -34,7 +34,7 @@ public class ANN {
         double y2 ; //hasil summing function output
         double[] yy = new double[hn]; //fungsi aktifasi
         double[] b = new double[hn]; //bias hidden
-        double output; //output 
+        double output=0; //output 
         double boutput; //bias output
         int baris = 1210;
         int kolom = 7;
@@ -45,7 +45,9 @@ public class ANN {
         String[][] train = new String[kolom][baris];
         int maxEpoch = 10;
         int epoch = 0;
-        int x1, x2, x3, x4, x5, x6,target; //buat input summing function     
+        int x1, x2, x3, x4, x5, x6,target; //buat input summing function    
+        double d2,db2;
+        double [] d1 = new double[hn]; //menghitung d1
         //ambil data
         try {
 
@@ -163,9 +165,9 @@ public class ANN {
            // output 
         for (int i = 0; i < baris; i++) {
             for (int j = 0; j < kolom; j++) {
-                System.out.print(data[j][i]+" ");
+              //  System.out.print(data[j][i]+" ");
             }
-            System.out.println(" ");
+           // System.out.println(" ");
         }
         //bobot ke hidden layer
         double[] w1 = new double[hn];  //dari input 1
@@ -174,7 +176,20 @@ public class ANN {
         double[] w4 = new double[hn];  //dari input 4 
         double[] w5 = new double[hn];  //dari input 5
         double[] w6 = new double[hn];  //dari input 6
-
+        
+        //delta bobot dan bias
+        double[] dw1 = new double[hn];  //new bobot  1
+        double[] dw2 = new double[hn];  //new bobot  2
+        double[] dw3 = new double[hn];  //new bobot  3
+        double[] dw4 = new double[hn];  //new bobot  4 
+        double[] dw5 = new double[hn];  //new bobot  5
+        double[] dw6 = new double[hn];  //new bobot  6
+        double[] dwoutput = new double[hn];
+        double[] db = new double[hn]; 
+        
+       
+        
+        
         //bobot ke output layer 
         double[] Woutput = new double[hn];
 
@@ -199,9 +214,9 @@ public class ANN {
         boutput = r.nextDouble();
 
         //start hitung maju
-        while (epoch < 1) {
+        while (epoch < 100) {
             //tampung data ke variabel
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 6; i++) {
                 x1 = data[0][i];
                 x2 = data[1][i];
                 x3 = data[2][i];
@@ -219,13 +234,13 @@ public class ANN {
                 //hitung maju sebanyak jumlah neuron
                 for (int j = 0; j < hn; j++) {
                  //hidden layer
-                    System.out.println(" intput ");
-                    System.out.println(" x1 : " +x1 + " x2 :  " +x2 + " x3 : "+x3 +" x4 : " +x4+" x5 : "+x5 +" x6 : " +x6  );
-                    System.out.println(" bobot ");
-                    System.out.println("1 : " +w1[j]+ " 2 : " + w2[j]+ " 3 : " +w3[j] + " 4 : "+ w4[j]+ " 5 : " + w5[j]+ " 6 : " + w6[j]);
+//                    System.out.println(" intput ");
+//                    System.out.println(" x1 : " +x1 + " x2 :  " +x2 + " x3 : "+x3 +" x4 : " +x4+" x5 : "+x5 +" x6 : " +x6  );
+//                    System.out.println(" bobot ");
+//                    System.out.println("1 : " +w1[j]+ " 2 : " + w2[j]+ " 3 : " +w3[j] + " 4 : "+ w4[j]+ " 5 : " + w5[j]+ " 6 : " + w6[j]);
                     y[j]=((x1*w1[j])+(x2*w2[j]+(x3*w3[j])+ (x4*w4[j]) + (x5*w5[j])+ (x6*w6[j]) +b[j])); //summing function
                     yy[j]=(1/(1+Math.exp(-(y[j]))));  //fungsi aktifasi
-                    System.out.println("iterasi ke "+(1+i)+" ativation function neuron ke "+(j+1)+" "+yy[j]);
+            //        System.out.println("iterasi ke "+(1+i)+" ativation function neuron ke "+(j+1)+" "+yy[j]);
                  }
                  //output layer 
                 double temp=0;
@@ -233,19 +248,60 @@ public class ANN {
                  for (int k = 0; k < hn; k++) {
                     temp=yy[k]*Woutput[k]+temp;
                 }
-                System.out.println(" hasil temp "+temp); 
+         //       System.out.println(" hasil temp "+temp); 
                 y2 = temp + boutput;
                 output = (1/(1+Math.exp(-y2)));
-                System.out.println(" summing function output "+y2);
-                System.out.println(" fungsi aktifasi "+output);
+//                System.out.println(" summing function output "+y2);
+//                System.out.println(" fungsi aktifasi "+output);
                 error = target-output;
                 error2 = error2 + Math.pow(error, 2);
-                System.out.println(" error " +error);
+            //    System.out.println(" error " +error);
                 
+                //perhitungan mundur
+                
+                //hitung d2 (delta di output) 
+                d2=(1-(Math.pow(output, 2)))*error;
+            //    System.out.println(" d2 done ");
+                
+                //menghitung d1
+                 for (int l = 0; l < hn; l++) {
+                    d1[l]=(1-(Math.pow(yy[l], 2)))*(Woutput[l]*d2);
+                }
+              //   System.out.println(" d1 done ");
+                
+                 //menghitung bobot baru
+                 for (int x = 0; x < hn; x++) {
+                    dw1[x]=alpha*d1[x]*x1;
+                    dw2[x]=alpha*d1[x]*x2;
+                    dw3[x]=alpha*d1[x]*x3;
+                    dw4[x]=alpha*d1[x]*x4;
+                    dw5[x]=alpha*d1[x]*x5;
+                    dw6[x]=alpha*d1[x]*x6;
+                    dwoutput[x]=alpha*d2*yy[x];
+                    db[x]=alpha*d1[x];
+                }
+                 db2=alpha*d2;
+                 
+                 //update bobot
+                 
+                 for (int w = 0; w < hn; w++) {
+                    w1[w]=w1[w]+dw1[w];
+                    w2[w]=w2[w]+dw2[w];
+                    w3[w]=w3[w]+dw3[w];
+                    w4[w]=w4[w]+dw4[w];
+                    w5[w]=w5[w]+dw5[w];
+                    w6[w]=w6[w]+dw6[w];
+                    b[w]=b[w]+db[w];    
+                }
+                
+                //update bias output 
+                 output = output + db2;
+                 
             }
             
 
-
+            MSE=output/baris;
+            System.out.println((epoch+1)+ "   MSE : "+MSE );
             epoch++;
         }
 
